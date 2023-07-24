a27d47f81d172408db70e7cd38e652694be96a01
AbdulDeBorja09 <abduldb09@gmail.com>
2023-07-25 07:05:16 +0800

    login

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 // SAMPLE LOGIN
-function valdiate (){
+function valdiate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  var user = "admin";
-  var pass = "silakbo123";
-
-  if(username == user && password == pass){
-    alert("login succes")
-  }else{
-    alert("login failed")
+  if (username == "admin" && password == "silakbo123") {
+    alert("login success");
+  } else {
+    alert("login failed");
   }
-
-
-
 }
