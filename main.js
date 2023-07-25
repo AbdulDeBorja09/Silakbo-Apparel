a1ef06c6bcc6f1a19958addb2c54da38010e057e
AbdulDeBorja09 <abduldb09@gmail.com>
2023-07-26 06:37:19 +0800

    Login

--- main.js
+++ main.js
@@ -3,13 +3,11 @@ function validate() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
 
-  if (username == "admin" && password == "silakbo123") {
-    window.location.href =
-      "https://abduldeborja09.github.io/SilakboApparel/onsite.html";
+  if (username === "admin" && password === "silakbo123") {
+    window.location.href = "index.html#sectionhome";
   } else {
     alert("failed");
   }
 }
 
-
-
+// 
